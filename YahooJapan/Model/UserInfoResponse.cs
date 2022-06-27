07decbd830e394b0940a9116837a34c9f49b0808
Jeonghee Lee <jjoylee@users.noreply.github.com>
2022-06-28 08:30:36 +0900

    Create UserInfoResponse.cs

---
+++ YahooJapan/Model/UserInfoResponse.cs
@@ -0,0 +1,24 @@
+using Newtonsoft.Json.Linq;
+
+namespace JangBoGo.Owin.Security.YahooJapan.Model
+{
+    public class UserInfoResponse
+    {
+        public UserInfoResponse(JObject response)
+        {
+            Response = response;
+            Id = TryGetValue("sub");
+            Email = TryGetValue("email");
+        }
+
+        public JObject Response { get; }
+        public string Id { get; }
+        public string Email { get; }
+
+        private string TryGetValue(string propertyName)
+        {
+            JToken value;
+            return Response.TryGetValue(propertyName, out value) ? value.ToString() : null;
+        }
+    }
+}
