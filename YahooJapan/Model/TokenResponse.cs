327907f09328faa65b12f6ce2f8c857486764c51
Jeonghee Lee <jjoylee@users.noreply.github.com>
2022-06-28 08:30:52 +0900

    Create TokenResponse.cs

---
+++ YahooJapan/Model/TokenResponse.cs
@@ -0,0 +1,18 @@
+using Newtonsoft.Json.Linq;
+
+namespace JangBoGo.Owin.Security.YahooJapan.Model
+{
+    public class TokenResponse
+    {
+        public TokenResponse(JObject response)
+        {
+            AccessToken = response["access_token"].Value<string>();
+            RefreshToken = response["refresh_token"].Value<string>();
+            ExpiresIn = response["expires_in"].Value<string>();
+        }
+
+        public string AccessToken { get; }
+        public string RefreshToken { get; }
+        public string ExpiresIn { get; }
+    }
+}
