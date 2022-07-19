d86c4ed65c7d5b2d73d25e96d3121d9c92703b93
Jeonghee Lee <jjoylee@users.noreply.github.com>
2022-07-19 23:34:53 +0900

    Update CartGroupView.cs

--- Portfolio/TODO_Cart/Code/CartGroupView.cs
+++ Portfolio/TODO_Cart/Code/CartGroupView.cs
@@ -1,5 +1,8 @@
+// 카트 페이지에서 같이 주문할 수 있는 상품 구분(그룹핑)해서 보여주기 위한 view model
 public class CartGroupView
 {
     public string Key { get; set; } = string.Empty;
-    public IList<CartItemUserView> List { get; set; } = new List<CartItemUserView>();
+    
+    // 해당 그룹(KEY로 구분)의 카트 상품 리스트
+    public IList<CartItemUserView> List { get; set; } = new List<CartItemUserView>(); 
 }
