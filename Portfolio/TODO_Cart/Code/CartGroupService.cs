caff2c19961a7ca3fee3bcc82999af99074e15f4
Jeonghee Lee <jjoylee@users.noreply.github.com>
2022-07-19 23:33:11 +0900

    Update CartGroupService.cs

--- Portfolio/TODO_Cart/Code/CartGroupService.cs
+++ Portfolio/TODO_Cart/Code/CartGroupService.cs
@@ -3,11 +3,13 @@ public class CartGroupService : ICartGroupService
     [Autowire]
     public IGroupMaker GroupMaker { get; set; }
 
+    // 카트에서 같이 주문할 수 있는 상품들끼리 grouping 
     public IList<CartGroupView> Grouping(IList<CartItemUserView> list)
     {
         return GroupMaker.Make(list);
     }
 
+    // 모든 상품 다 같이 주문할 수 있으면 true > 즉 group이 하나.
     public bool AllSameGroup(IList<CartItemUserView> list)
     {
         return Grouping(list).Count() == 1;
