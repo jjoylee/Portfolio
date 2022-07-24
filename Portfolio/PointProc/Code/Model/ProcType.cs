fa13b0e16d97a3534f888f24cdc67e82138a0ab1
jjoylee <ekdnlt1995@gmail.com>
2022-07-24 17:50:37 +0900

    add comment at proctype file

--- Portfolio/PointProc/Code/Model/ProcType.cs
+++ Portfolio/PointProc/Code/Model/ProcType.cs
@@ -1,7 +1,14 @@
-    public enum ProcType
-    {
-        GENERALPROC,
-        DEDUCTIONPOINTPROC,
-        RETURNPOINTPROC,
-        CANNOTPROC
-    }
+/**
+    입금 처리 CASE 분리
+    GENERALPROC : 총 주문금액 == 입금액
+    RETURNPOINTPROC : 총 주문금액 < 입금액
+    DEDUCTIONPOINTPROC : 입금액 < 총 주문금액 < 입금액 + 고객이 보유한 포인트
+    CANNOTPROC : 입금액 < 입금액 + 고객이 보유한 포인트 < 총 주문금액
+**/
+public enum ProcType
+{
+    GENERALPROC,
+    RETURNPOINTPROC,
+    DEDUCTIONPOINTPROC,
+    CANNOTPROC
+}
