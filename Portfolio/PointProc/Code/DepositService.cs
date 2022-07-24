cd6c8c84efb294f78a70b08ac55bb9cf94f18211
jjoylee <ekdnlt1995@gmail.com>
2022-07-24 22:14:15 +0900

    code line 정리중

--- Portfolio/PointProc/Code/DepositService.cs
+++ Portfolio/PointProc/Code/DepositService.cs
@@ -10,16 +10,19 @@ public class DepositService : IDepositService
     [Autowire]
     public IDepositProcInfoService DepositProcInfoService { get; set; }
 
+    // 입금 처리
     public void ConfirmDeposit(int id, string orderIds, string memo, DepositMatchingInfo submitInfo, string adminId)
     {
         DepositConfirmService.ConfirmDeposit(id, orderIds, memo, submitInfo, adminId);
     }
 
+    // 입금 메모, 주문번호 update
     public void SaveDeposit(int id, string orderIds, string memo)
     {
         DepositSaveService.SaveDeposit(id, orderIds, memo);
     }
 
+    // prev 내용 가져오기
     public DepositProcInfo GetDepositProcInfo(int id, string orderIds)
     {
         return DepositProcInfoService.GetDepositProcInfo(id, orderIds);
