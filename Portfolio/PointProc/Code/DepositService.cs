248602c36221e2b5821974b444f6e192d169ff8d
Jeonghee Lee <ekdnlt1995@gmail.com>
2022-07-25 01:06:00 +0900

    Update DepositService.cs

--- Portfolio/PointProc/Code/DepositService.cs
+++ Portfolio/PointProc/Code/DepositService.cs
@@ -26,6 +26,7 @@ public class DepositService : IDepositService
         입금 처리 전 주문금액, 고객 포인트 조회, 입금 처리 가능한지 상태 파악 (적용버튼 클릭)
         적용 버튼을 누르면 DepositProcInfo 객체를 만들어 return
         입금 처리할 때(처리완료/포인트처리 버튼 클릭) ConfirmDeposit의 submitInfo로 넘김
+        GetDepositProcInfo → ConfirmDeposit
     **/
     public DepositProcInfo GetDepositProcInfo(int id, string orderIds)
     {
